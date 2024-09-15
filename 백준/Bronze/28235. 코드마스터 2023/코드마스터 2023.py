f379b7f057ded50a394af8eeafd9d9ffe4b49270
Wonjae Jo <inqueue0979@gmail.com>
2024-09-16 03:26:48 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+from sys import stdin
+datum = stdin.readline().rstrip()
+if datum == "SONGDO":
+    print("HIGHSCHOOL")
+elif datum == "CODE":
+    print("MASTER")
+elif datum == "2023":
+    print("0611")
+else:
+    print("CONTEST")
